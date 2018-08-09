5eda8e0c0cf398c9dfcfa74ea1313cade55a677e
plwsdytlcj <cl2445@cornell.edu>
2018-08-09 11:48:47 +0800

    Fix it

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,5 +1,6 @@
 package com.example;
 
+
 /**
  * This is a class.
  */
@@ -11,9 +12,11 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * @param name
+   * @return greeting string
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
